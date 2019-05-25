1f3f486bdeb1280ca3ad1546830304ffe7eafaf8
Anwesh43 <anweshthecool0@gmail.com>
2019-05-25 15:04:51 +0530

    adding CrossLineStepStage class

--- CrossLineStepStage.ts
+++ CrossLineStepStage.ts
@@ -73,3 +73,36 @@ class DrawingUtil {
         context.restore()
     }
 }
+
+class CrossLineStepStage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = backColor
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+            this.canvas.onmousedown = () => {
+
+            }
+        }
+    }
+
+    static init() {
+        const stage : CrossLineStepStage = new CrossLineStepStage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
+    }
+}
