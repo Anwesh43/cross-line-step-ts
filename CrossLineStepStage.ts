282ff244217c0e7613d8e71a42293b020f571824
Anwesh43 <anweshthecool0@gmail.com>
2019-05-25 14:44:23 +0530

    adding ScaleUtil class

--- CrossLineStepStage.ts
+++ CrossLineStepStage.ts
@@ -8,3 +8,27 @@ const strokeFactor : number = 90
 const sizeFactor : number = 2.9
 const foreColor : string = "#1565C0"
 const backColor : string = "#BDBDBD"
+
+class ScaleUtil {
+
+    static scaleFactor(scale : number) : number {
+        return scale / scDiv
+    }
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+
+    static mirrorValue(scale : number, a : number, b : number) : number {
+        const k : number = ScaleUtil.scaleFactor(scale)
+        return (1 - k) / a + k / b
+    }
+
+    static updateValue(scale : number, dir : number, a : number, b : number) : number {
+        return ScaleUtil.mirrorValue(scale, a, b) * dir * scGap
+    }
+}
